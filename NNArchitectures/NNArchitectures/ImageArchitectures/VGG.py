06cb4fe8f62717817a24eea3f19498769fad8126
SebChw <sebastian.chwilczynski@gmail.com>
2022-04-25 14:07:11 +0200

    Update VGG

--- NNArchitectures/NNArchitectures/ImageArchitectures/VGG.py
+++ NNArchitectures/NNArchitectures/ImageArchitectures/VGG.py
@@ -12,14 +12,14 @@ class VGGsection(nn.Module):
         self.conv_padding = 1
         self.pool_size = 2
         self.pool_stride = 2
-
+       
         for i in range(num_of_blocks):
             self.blocks.append( nn.Sequential(
                 torch.nn.Conv2d(previous_num_of_filters, current_num_of_filters, self.kernel_size, self.conv_stride, self.conv_padding),
                 torch.nn.BatchNorm2d(current_num_of_filters),
                 torch.nn.ReLU()
             ))
-            previous_num_of_filters = current_num_of_filters
+            previous_num_of_filters = current_num_of_filters # as at first iteration we increase num of filters and then it stays the same
         
         self.blocks.append(torch.nn.MaxPool2d(self.pool_size, self.pool_stride))
 
@@ -67,13 +67,14 @@ class VGG(nn.Module):
 
     def calculate_flattened_size(self, input_size=224):
         #In case of VGG only maxpoolings decrease spatial dimension by a factor of 2 and each VGG has 5 max pooling layers
-        end_num_of_channels = 512
+        self.end_num_of_channels = 64 * 2 ** (len(self.num_of_blocks) - 2)
+        #print(len(self.num_of_blocks))
         for i in range(len(self.num_of_blocks)):
             kernel, stride = (2,2)
             input_size = int((input_size - kernel) / stride + 1)
             
 
-        self.flattened_size = input_size ** 2 * end_num_of_channels
+        self.flattened_size = input_size ** 2 * self.end_num_of_channels
 
 def get_VGG16(num_of_classes, input_size, num_of_channels):
     return VGG(num_of_classes, [2,2,3,3,3], input_size, num_of_channels)
@@ -84,5 +85,6 @@ def get_VGG19(num_of_classes, input_size, num_of_channels):
 
 if __name__ == '__main__':
     vgg = get_VGG16(100, 224, 3)
+    print(vgg.end_num_of_channels)
     input = torch.randn(1,3,224,224)
     print(vgg(input).shape)
